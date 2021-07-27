f3c5969b8945f052db8c4a19f345927f152fbdf1
MaxH1996 <80034375+MaxH1996@users.noreply.github.com>
2021-07-27 15:06:10 +0200

    representation

---
+++ representation/painn.py
@@ -0,0 +1,208 @@
+from typing import Callable, Dict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import schnetpack.properties as structure
+import schnetpack.nn as snn
+
+from schnetpack.representation.support import Residual, Output, Module, PhysNetCutOff
+
+__all__ = ["PaiNN"]
+
+
+class PaiNNInteraction(nn.Module):
+    r"""PaiNN interaction block for modeling equivariant interactions of atomistic systems."""
+
+    def __init__(
+        self,
+        n_atom_basis: int,
+        activation: Callable,
+        epsilon: float = 1e-8,
+    ):
+        """
+        Args:
+            n_atom_basis: number of features to describe atomic environments.
+            activation: if None, no activation function is used.
+            epsilon: stability constant added in norm to prevent numerical instabilities
+        """
+        super(PaiNNInteraction, self).__init__()
+        self.n_atom_basis = n_atom_basis
+
+        self.interatomic_context_net = nn.Sequential(
+            snn.Dense(n_atom_basis, n_atom_basis, activation=activation),
+            snn.Dense(n_atom_basis, 3 * n_atom_basis, activation=None),
+        )
+        self.intraatomic_context_net = nn.Sequential(
+            snn.Dense(2 * n_atom_basis, n_atom_basis, activation=activation),
+            snn.Dense(n_atom_basis, 3 * n_atom_basis, activation=None),
+        )
+        self.mu_channel_mix = snn.Dense(n_atom_basis, 2 * n_atom_basis, activation=None)
+        self.epsilon = epsilon
+
+    def forward(
+        self,
+        q: torch.Tensor,
+        mu: torch.Tensor,
+        Wij: torch.Tensor,
+        dir_ij: torch.Tensor,
+        idx_i: torch.Tensor,
+        idx_j: torch.Tensor,
+        n_atoms: int,
+    ):
+        """Compute interaction output.
+
+        Args:
+            q: scalar input values
+            mu: vector input values
+            Wij: filter
+            idx_i: index of center atom i
+            idx_j: index of neighbors j
+
+        Returns:
+            atom features after interaction
+        """
+        # inter-atomic
+        x = self.interatomic_context_net(q)
+        xj = x[idx_j]
+        muj = mu[idx_j]
+        x = Wij * xj
+
+        dq, dmuR, dmumu = torch.split(x, self.n_atom_basis, dim=-1)
+        dq = snn.scatter_add(dq, idx_i, dim_size=n_atoms)
+        dmu = dmuR * dir_ij[..., None] + dmumu * muj
+        dmu = snn.scatter_add(dmu, idx_i, dim_size=n_atoms)
+
+        q = q + dq
+        mu = mu + dmu
+
+        ## intra-atomic
+        mu_mix = self.mu_channel_mix(mu.float())
+        mu_V, mu_W = torch.split(mu_mix, self.n_atom_basis, dim=-1)
+        mu_Vn = torch.sqrt(torch.sum(mu_V ** 2, dim=-2, keepdim=True) + self.epsilon)
+
+        ctx = torch.cat([q, mu_Vn], dim=-1)
+        x = self.intraatomic_context_net(ctx)
+
+        dq_intra, dmu_intra, dqmu_intra = torch.split(x, self.n_atom_basis, dim=-1)
+        dmu_intra = dmu_intra * mu_W
+
+        dqmu_intra = dqmu_intra * torch.sum(mu_V * mu_W, dim=1, keepdim=True)
+
+        q = dq_intra + dqmu_intra
+        mu = dmu_intra
+        return q, mu
+
+
+class PaiNN(nn.Module):
+    """PaiNN - polarizable interaction neural network
+
+    References:
+
+    .. [#painn1] Schütt, Unke, Gastegger:
+       Equivariant message passing for the prediction of tensorial properties and molecular spectra.
+       ICML 2021 (to appear)
+
+    """
+
+    def __init__(
+        self,
+        n_atom_basis: int,
+        n_interactions: int,
+        radial_basis: nn.Module,
+        cutoff_fn: Callable,
+        activation=F.silu,
+        max_z: int = 100,
+        shared_interactions: bool = False,
+        shared_filters: bool = False,
+        epsilon: float = 1e-8,
+    ):
+        """
+        Args:
+            n_atom_basis: number of features to describe atomic environments.
+                This determines the size of each embedding vector; i.e. embeddings_dim.
+            n_interactions: number of interaction blocks.
+            radial_basis: layer for expanding interatomic distances in a basis set
+            cutoff_fn: cutoff function
+            activation: activation function
+            shared_interactions: if True, share the weights across
+                interaction blocks.
+            shared_interactions: if True, share the weights across
+                filter-generating networks.
+            epsilon: stability constant added in norm to prevent numerical instabilities
+        """
+        super(PaiNN, self).__init__()
+
+        self.n_atom_basis = n_atom_basis
+        self.n_interactions = n_interactions
+        self.cutoff_fn = cutoff_fn
+        self.radial_basis = radial_basis
+
+        self.embedding = nn.Embedding(max_z, n_atom_basis, padding_idx=0)
+
+        self.share_filters = shared_filters
+
+        if shared_filters:
+            self.filter_net = snn.Dense(
+                self.radial_basis.n_rbf, 3 * n_atom_basis, activation=None
+            )
+        else:
+            self.filter_net = snn.Dense(
+                self.radial_basis.n_rbf,
+                self.n_interactions * n_atom_basis * 3,
+                activation=None,
+            )
+
+        self.interactions = snn.replicate_module(
+            lambda: PaiNNInteraction(
+                n_atom_basis=self.n_atom_basis,
+                activation=activation,
+                epsilon=epsilon,
+            ),
+            self.n_interactions,
+            shared_interactions,
+        )
+
+    def forward(self, inputs: Dict[str, torch.Tensor]):
+        """
+        Compute atomic representations/embeddings.
+
+        Args:
+            inputs (dict of torch.Tensor): SchNetPack dictionary of input tensors.
+
+        Returns:
+            torch.Tensor: atom-wise representation.
+            list of torch.Tensor: intermediate atom-wise representations, if
+            return_intermediate=True was used.
+        """
+        # get tensors from input dictionary
+        atomic_numbers = inputs[structure.Z]
+        r_ij = inputs[structure.Rij]
+        idx_i = inputs[structure.idx_i]
+        idx_j = inputs[structure.idx_j]
+        n_atoms = atomic_numbers.shape[0]
+
+        # compute atom and pair features
+        d_ij = torch.norm(r_ij, dim=1, keepdim=True).float()
+        dir_ij = r_ij / d_ij
+        phi_ij = self.radial_basis(d_ij)
+        fcut = self.cutoff_fn(d_ij)
+
+        filters = self.filter_net(phi_ij) * fcut[..., None]
+        if self.share_filters:
+            filter_list = [filters] * self.n_interactions
+        else:
+            filter_list = torch.split(filters, 3 * self.n_atom_basis, dim=-1)
+
+        q = self.embedding(atomic_numbers)[:, None]
+        qs = q.shape
+        mu = torch.zeros((qs[0], 3, qs[2]), device=q.device)
+
+        for i, interaction in enumerate(self.interactions):
+            q, mu = interaction(q.float(), mu.float(), filter_list[i], dir_ij, idx_i, idx_j, n_atoms)
+
+        q = q.squeeze(1)
+
+        return {"scalar_representation": q, "vector_representation": mu}
+ 
